e6b590f6568abcbab3b4eb5878efe53ae91d1f8a
Sieun Choi <qdial214@dgu.ac.kr>
2022-11-19 16:30:08 +0900

    Update 1954.py
    - 코드 설명을 위한 주석 추가

--- 1954.py
+++ 1954.py
@@ -3,9 +3,13 @@ test_case=[]
 for i in range(T):
 
     N = int(input())
+    # 인덱스를 활용해서 방향전환을 하고 싶을때는 
+    # delta row(dr), delta column(dc) 리스트를 만들고 접근하면 된다
+
     # 방향: 우->하->좌->상
-    dir_row = [0, 1, 0, -1]
-    dir_col = [1, 0, -1, 0]
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+
     snail = [[0 for j in range(N)] for i in range(N)]
 
     r = 0
@@ -15,15 +19,18 @@ for i in range(T):
 
         snail[r][c] = num
 
-        r += dir_row[dist]
-        c += dir_col[dist]
+        r += dr[dist]
+        c += dc[dist]
 
         if r > N - 1 or c > N - 1 or snail[r][c] != 0:
-            r -= dir_row[dist]
-            c -= dir_col[dist]
+            # 인덱스 원위치
+            r -= dr[dist]
+            c -= dc[dist]
+            # dist를 0,1,2,3 내의 값으로 유지
             dist = (dist + 1) % 4
-            r += dir_row[dist]
-            c += dir_col[dist]
+            # 방향 전환 (dist+1)
+            r += dr[dist]
+            c += dc[dist]
     test_case.append(snail)
 
 i=0
